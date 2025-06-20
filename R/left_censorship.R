f88011767af792cf16f94210415f59699fe4fee5
rmflight <r.flight79@gmail.com>
2025-06-20 09:59:22 -0400

    handling the case when there are no missing values

--- R/left_censorship.R
+++ R/left_censorship.R
@@ -1,153 +1,181 @@
 #' Test for left censorship
-#' 
+#'
 #' Does a binomial test to check if the most likely cause of missing values
-#' is due to values being below the limit of detection, or coming from a 
+#' is due to values being below the limit of detection, or coming from a
 #' left-censored distribution.
-#' 
+#'
 #' @param data_matrix matrix or data.frame of numeric data
 #' @param global_na what represents zero or missing?
 #' @param sample_classes which samples are in which class
-#' 
+#'
 #' @details
 #' For each feature that is missing in a group of samples, we save as a possibility
 #' to test. For each sample, we calculate the median value with any missing values
-#' removed. Each feature that had a missing value, we test whether the remaining 
-#' non-missing values are below the sample median for those samples where the 
+#' removed. Each feature that had a missing value, we test whether the remaining
+#' non-missing values are below the sample median for those samples where the
 #' feature is non-missing. A binomial test considers the total number of features
 #' instances (minus missing values) as the number of trials, and the number of
 #' of features below the sample medians as the number of successes.
-#' 
+#'
 #' There is a bit more detail in the vignette: `vignette("testing-for-left-censorship", package = "ICIKendallTau")`
-#' 
+#'
 #' @seealso [ici_kendalltau()]
-#' 
+#'
 #' @examples
 #' # this example has 80% missing due to left-censorship
 #' data(missing_dataset)
 #' missingness = test_left_censorship(missing_dataset)
 #' missingness$values
 #' missingness$binomial_test
-#' 
+#'
 #' @importFrom stats median
-#' 
+#'
 #' @export
 #' @return data.frame of trials / successes, and binom.test result
-test_left_censorship = function(data_matrix, 
-                                global_na = c(NA, Inf, 0),
-                                sample_classes = NULL)
-{
+test_left_censorship = function(
+  data_matrix,
+  global_na = c(NA, Inf, 0),
+  sample_classes = NULL
+) {
   if (inherits(data_matrix, "data.frame")) {
     data_matrix = as.matrix(data_matrix)
   }
   if (is.null(sample_classes)) {
     sample_classes = rep("A", ncol(data_matrix))
   }
-  
+
   split_indices = split(seq_len(ncol(data_matrix)), sample_classes)
   missing_loc = setup_missing_matrix(data_matrix, global_na)
+
+  if (sum(missing_loc) == 0) {
+    arg = rlang::caller_arg(data_matrix)
+    call = rlang::caller_env()
+    cli::cli_inform(
+      message = c(
+        'i' = '{.arg {arg}} has no missing values, returning NULL'
+      )
+    )
+    return(NULL)
+  }
+
   data_matrix_missing = data_matrix
   data_matrix_missing[missing_loc] = NA
-  
+
   # split the dataset by group
-  split_counts = purrr::imap(split_indices, \(in_split, split_id){
+  split_counts = purrr::imap(split_indices, \(in_split, split_id) {
     # in_split = split_indices[[1]]
-    
+
     # grab the group we want to work with
     split_missing = data_matrix_missing[, in_split, drop = FALSE]
-    
+
     # count the number of missing samples for each feature,
     # and keep those that have at least one
     n_miss = rowSums(is.na(split_missing))
-    
+
     if (sum(n_miss) == 0) {
       out_res = data.frame(trials = 0, success = 0, class = split_id)
       return(out_res)
     }
-    
+
     keep_miss = split_missing[n_miss > 0, , drop = FALSE]
-    
+
     # get sample medians
-    sample_medians = calculate_matrix_medians(split_missing, use = "col", na.rm = TRUE)
-    
+    sample_medians = calculate_matrix_medians(
+      split_missing,
+      use = "col",
+      na.rm = TRUE
+    )
+
     # turn the medians into a matrix to make life easier
-    median_matrix = matrix(sample_medians, nrow = nrow(keep_miss),
-                           ncol = ncol(keep_miss), byrow = TRUE)
+    median_matrix = matrix(
+      sample_medians,
+      nrow = nrow(keep_miss),
+      ncol = ncol(keep_miss),
+      byrow = TRUE
+    )
     # do the comparison
     keep_miss_updown = keep_miss < median_matrix
-    
+
     # count how many trials we ran, and how many successes we have
-    all_trials = (nrow(keep_miss_updown) * ncol(keep_miss_updown)) - sum(is.na(keep_miss_updown))
+    all_trials = (nrow(keep_miss_updown) * ncol(keep_miss_updown)) -
+      sum(is.na(keep_miss_updown))
     all_success = sum(keep_miss_updown, na.rm = TRUE)
-    
-    out_res = data.frame(trials = all_trials, success = all_success, class = split_id)
+
+    out_res = data.frame(
+      trials = all_trials,
+      success = all_success,
+      class = split_id
+    )
     return(out_res)
   }) |>
     purrr::list_rbind()
-  
-  
+
   total_trials = sum(split_counts$trials)
   total_success = sum(split_counts$success)
-  
-  binom_res = stats::binom.test(total_success, total_trials, p = 0.5, alternative = "greater")
-  
+
+  binom_res = stats::binom.test(
+    total_success,
+    total_trials,
+    p = 0.5,
+    alternative = "greater"
+  )
+
   if (is.null(sample_classes)) {
     total_success$class = NULL
   }
-  
-  return(list(values = split_counts,
-              binomial_test = binom_res))
+
+  return(list(values = split_counts, binomial_test = binom_res))
 }
 
 
 #' Calculate matrix medians
-#' 
+#'
 #' Given a matrix of data, calculates the median value in each column or row.
-#' 
+#'
 #' @param in_matrix numeric matrix of values
 #' @param use character of "col" or "row" defining columns or rows
 #' @param ... extra parameters to the median function
-#' 
+#'
 #' @export
 #' @return numeric
-calculate_matrix_medians = function(in_matrix, use = "col", ...)
-{
+calculate_matrix_medians = function(in_matrix, use = "col", ...) {
   if (use %in% "row") {
     in_matrix = t(in_matrix)
-  } 
-  out_medians = purrr::map_dbl(seq_len(ncol(in_matrix)), \(in_col){
+  }
+  out_medians = purrr::map_dbl(seq_len(ncol(in_matrix)), \(in_col) {
     stats::median(in_matrix[, in_col], ...)
   })
   return(out_medians)
 }
 
 #' Add uniform noise
-#' 
+#'
 #' Adds uniform noise to values, generating replicates with noise added to
 #' the original.
-#' 
+#'
 #' @param value a single or vector of numeric values
 #' @param n_rep the number of replicates to make (numeric). Default is 1.
 #' @param sd the standard deviation of the data
 #' @param use_zero logical, should returned values be around zero or not?
-#' 
+#'
 #' @export
-#' 
+#'
 #' @return numeric matrix
-add_uniform_noise = function(value, n_rep, sd, use_zero = FALSE){
+add_uniform_noise = function(value, n_rep, sd, use_zero = FALSE) {
   n_value = length(value)
-  
+
   n_sd = n_rep * n_value
-  
+
   out_sd = stats::rnorm(n_sd, 0, sd)
   out_sd = matrix(out_sd, nrow = n_value, ncol = n_rep)
-  
+
   if (!use_zero) {
     tmp_value = matrix(value, nrow = n_value, ncol = n_rep, byrow = FALSE)
     out_value = tmp_value + out_sd
   } else {
     out_value = out_sd
   }
-  
+
   return(out_value)
 }
 
@@ -158,18 +186,18 @@ add_uniform_noise = function(value, n_rep, sd, use_zero = FALSE){
 #' @format ## `missing_dataset`
 #' A matrix with 1000 rows and 20 columns, where rows are features and
 #' columns are samples.
-#' 
+#'
 #' @source Robert M Flight
 "missing_dataset"
 
 #' Example RNA-Seq Dataset With Missingness
-#' 
+#'
 #' An example dataset from RNA-seq experiment on yeast, created by Gierliński et al.,
 #' "Statistical models for RNA-seq data derived from a two-condition 48-replicate experiment",
 #' Bioinformatics, 31, 2015 <https://doi.org/10.1093/bioinformatics/btv425>.
-#' 
+#'
 #' @format ## `yeast_missing`
 #' A matrix with 6887 rows (genes) and 96 columns (samples).
-#' 
+#'
 #' @source <https://dx.doi.org/10.6084/M9.FIGSHARE.1425502.V1> <https://dx.doi.org/10.6084/M9.FIGSHARE.1425503.V1>
 "yeast_missing"
--- tests/testthat/test-left_censorship.R
+++ tests/testthat/test-left_censorship.R
@@ -5,52 +5,72 @@ test_that("test_left_censorship works", {
   n_low = 80
   test_dataset = withr::with_seed(1234, rlnorm(n_feature, 10, 1))
   test_dataset = sort(test_dataset)
-  
-  noisy_dataset = withr::with_seed(1234, add_uniform_noise(log(test_dataset), n_sample, 0.1))
+
+  noisy_dataset = withr::with_seed(
+    1234,
+    add_uniform_noise(log(test_dataset), n_sample, 0.1)
+  )
   sample_medians = calculate_matrix_medians(noisy_dataset)
-  
+
   # check that we get the counts we expect
-  # we are going to pick n_miss features, and of those, n_low will be less than 
+  # we are going to pick n_miss features, and of those, n_low will be less than
   # the medians, hopefully by ordering.
   low_indices = withr::with_seed(1234, sample(seq_len(300), n_low))
   hi_indices = withr::with_seed(1234, sample(seq(800, 1000), n_miss - n_low))
-  
+
   all_indices = c(low_indices, hi_indices)
-  
-  sample_indices = withr::with_seed(1234, sample(n_sample, n_miss, replace = TRUE))
-  
+
+  sample_indices = withr::with_seed(
+    1234,
+    sample(n_sample, n_miss, replace = TRUE)
+  )
+
   zero_dataset = noisy_dataset
   for (i_loc in seq_along(all_indices)) {
     zero_dataset[all_indices[i_loc], sample_indices[i_loc]] = 0
   }
-  
+
+  expect_message(test_left_censorship(noisy_dataset), "has no missing values")
+
   zero_binom = test_left_censorship(zero_dataset, global_na = c(0, NA))
-  
-  expect_equal(zero_binom$values$success / zero_binom$values$trials, n_low / n_miss)
-  
+
+  expect_equal(
+    zero_binom$values$success / zero_binom$values$trials,
+    n_low / n_miss
+  )
+
   na_dataset = zero_dataset
   na_dataset[zero_dataset == 0] = NA
-  
+
   na_binom = test_left_censorship(na_dataset, global_na = c(0, NA))
   expect_equal(na_binom$values$success / na_binom$values$trials, n_low / n_miss)
-  
+
   # check that zero and NA work the same
   expect_equal(zero_binom, na_binom)
-  
+
   na_loc = which(is.na(na_dataset))
   back_zero = withr::with_seed(1234, sample(na_loc, 20))
   mix_dataset = na_dataset
   mix_dataset[back_zero] = 0
   mix_binom = test_left_censorship(mix_dataset, global_na = c(0, NA))
   expect_equal(na_binom, mix_binom)
-  
-  single_group_indices = withr::with_seed(1234, sample(n_sample / 2, n_miss, replace = TRUE))
+
+  single_group_indices = withr::with_seed(
+    1234,
+    sample(n_sample / 2, n_miss, replace = TRUE)
+  )
   group_classes = rep(c("A", "B"), each = 10)
   group_dataset = noisy_dataset
   for (i_loc in seq_along(all_indices)) {
     group_dataset[all_indices[i_loc], single_group_indices[i_loc]] = 0
   }
-  group_binom = test_left_censorship(group_dataset, sample_classes = group_classes)
+  group_binom = test_left_censorship(
+    group_dataset,
+    sample_classes = group_classes
+  )
   expect_equal(nrow(group_binom$values), 2)
-  expect_equal(group_binom$values$success[1] / group_binom$values$trials[1], n_low / n_miss)
+  expect_equal(
+    group_binom$values$success[1] / group_binom$values$trials[1],
+    n_low / n_miss
+  )
 })
